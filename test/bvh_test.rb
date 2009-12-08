6d43c2c5a2c701e85dbddd6ec2c4287957ccd1c0
sinisterchipmunk <sinisterchipmunk@gmail.com>
2009-12-08 08:11:29 -0500

    Just refactoring tests a bit

--- test/bvh_test.rb
+++ test/bvh_test.rb
@@ -9,30 +9,26 @@ require 'bvh'
 class BvhTest < Test::Unit::TestCase
   context "The Bvh class" do
     should "import BVH files" do
-      Bvh.import("test_data/chinese.bvh")
-      Bvh.import("test_data/lady.bvh")
+      Bvh.import("test/data/in/karate.bvh")
     end
   end
 
   context "A Bvh instance" do
     setup do
-      @chinese = Bvh.import("test_data/chinese.bvh")
-      @lady = Bvh.import("test_data/lady.bvh")
+      @karate = Bvh.import("test/data/in/karate.bvh")
     end
 
     should "export to file" do
-      @chinese.export("test_output/chinese.bvh")
-      @lady.export("test_output/lady.bvh")
+      @karate.export("test/data/out/karate.bvh")
     end
 
     should "be able to import its own exports" do
-      Bvh.import("test_output/chinese.bvh")
-      Bvh.import("test_output/lady.bvh")
+      Bvh.import("test/data/out/karate.bvh")
     end
 
     should "be able to calculate the transform matrices" do
-      frames = @chinese.motion.frames
-      bone = @chinese.skeleton/"LeftWrist"
+      frames = @karate.motion.frames
+      bone = @karate.skeleton/"LeftWrist"
 
       # FIXME: i don't know what values to assert against
       frames.last.local_transform_matrix(bone)
@@ -42,9 +38,9 @@ class BvhTest < Test::Unit::TestCase
     should "be able to add time to frames" do
       # add 3 seconds, during which each bone's offset should move and rotate by 3 units
       # no idea what this would look like if rendered, it's just a test
-      frame_count = @chinese.frame_count
-      @chinese.motion.add_time(3, @chinese.motion.frames.last + 3)
-      assert_equal frame_count+90, @chinese.frame_count # 30 fps for 3 sec
+      frame_count = @karate.frame_count
+      @karate.motion.add_time(3, @karate.motion.frames.last + 3)
+      assert_equal frame_count+90, @karate.frame_count # 30 fps for 3 sec
     end
   end
 end
